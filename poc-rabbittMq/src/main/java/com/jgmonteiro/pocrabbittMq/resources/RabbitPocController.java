bb29a86861a98585f024b817891b7693bb8e6fb7
jgmonteiro1 <jgmoreira15@gmail.com>
2022-01-24 22:27:12 -0300

    Exchange Direct

--- poc-rabbittMq/src/main/java/com/jgmonteiro/pocrabbittMq/resources/RabbitPocController.java
+++ poc-rabbittMq/src/main/java/com/jgmonteiro/pocrabbittMq/resources/RabbitPocController.java
@@ -1,8 +1,14 @@
 package com.jgmonteiro.pocrabbittMq.resources;
 
 import com.jgmonteiro.pocrabbittMq.messaging.QueueSender;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +19,18 @@ public class RabbitPocController {
     @Autowired
     private  QueueSender queueSender;
 
-    @GetMapping
-    public String sendMessage(){
-        queueSender.send("Mensagem teste endpoint");
-        return "Bateu auqi";
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/{exchange}/{routingKey}")
+    public ResponseEntity<Void> sendMessage(@PathVariable(value = "exchange") final String exchange, @PathVariable(value = "routingKey") final String routingKey, @RequestBody final String message){
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @GetMapping
+    public ResponseEntity<String>  teste(){
+        return ResponseEntity.status(HttpStatus.OK).body("Batu");
+    }
 
 }
